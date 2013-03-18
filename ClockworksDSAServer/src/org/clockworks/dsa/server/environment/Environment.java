6e698360603af159a9534ac947e41b69e89ec14a
Hugh Lardner <lardnerl@tcd.ie>
2013-03-18 12:51:56 +0000

    Added body to constructor. Still needs to have imput stream split up

--- ClockworksDSAServer/src/org/clockworks/dsa/server/environment/Environment.java
+++ ClockworksDSAServer/src/org/clockworks/dsa/server/environment/Environment.java
@@ -1,9 +1,8 @@
 package org.clockworks.dsa.server.environment;
 
 
-import java.io.File;
-import java.io.InputStream;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 import org.clockworks.dsa.server.singletons.PingQueue;
 
@@ -20,8 +19,76 @@ public class Environment {
 	 */
 	public Environment(int id,InputStream inputFromHttp){
 		this.id = id;
-		int sizeOfPingQueue = PingQueue.sharedInstance().readSize();
-		// TODO a lot
+		// NEED TO SPLIT inputFromHttp IN TO PYTHON SCRIPT AND PARAMETERS STRING!!!!
+		this.setPythonScript(pythonScript);
+		//create 2d array of strings of parameters 
+		String byIdent[] = parameters.split(";;"); // Based on grammar used for
+													// string
+		String byArg[][] = new String[byIdent.length][];
+		for (int i = 0; i < byIdent.length; i++) {
+			String var[] = byIdent[i].split(";"); // Based on grammar used for
+													// string
+			byArg[i] = new String[var.length];
+			for (int j = 0; j < byArg[i].length; j++) {
+				byArg[i][j] = var[j];
+			}
+		}
+		//create all permutations of parameters
+		String[][] permuted = permute(byArg);
+		segments = new EnvironmentSegment[permuted.length];
+		File[] completedScripts = new File[permuted.length];
+
+		// Convert original file in to array so new lines can be inserted.
+		List<String> lines = new ArrayList<String>();
+		try (BufferedReader br = new BufferedReader(
+				new FileReader(pythonScript));) {
+
+			String sCurrentLine;
+			while ((sCurrentLine = br.readLine()) != null) {
+				lines.add(sCurrentLine);
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// Find end of imports section of file.
+		int insertAfterImports = 0;
+		while (lines.get(insertAfterImports).contains("import"))
+			insertAfterImports++;
+
+		// Create new competed python scripts
+		for (int i = 0; i < completedScripts.length; i++) {
+			// create file name
+			String[] tmpFileName = pythonScript.getName().split(".");
+			String fileName = tmpFileName[0] + i + "." + tmpFileName[1];
+			completedScripts[i] = new File(fileName);
+			// fill in file data
+			try (PrintStream out = new PrintStream(new FileOutputStream(
+					completedScripts[i]))) {
+				// imports section
+				for (int j = 0; j < insertAfterImports; j++) {
+					out.println(lines.get(j));
+				}
+				// new parameters
+				for (int j = 0; j < permuted[i].length; j++) {
+					out.println(permuted[i][j]);
+				}
+				// Remainder of original script
+				for (int j = insertAfterImports; j < lines.size(); j++) {
+					out.println(lines.get(j));
+				}
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		// Create all enviroment segments
+		for (int i = 0; i < segments.length; i++) {
+			segments[i] = new EnvironmentSegment(id, i, permuted[i],
+					completedScripts[i]);
+		}
 	}
 	
 	/**
@@ -61,6 +128,14 @@ public class Environment {
 		return this.id;
 	}
 	
+	public File getPythonScript() {
+		return pythonScript;
+	}
+
+	private void setPythonScript(File pythonScript) {
+		this.pythonScript = pythonScript;
+	}
+	
 	/**
 	 * Return true if all the segments of this simulation have results associated
 	 * @return
@@ -85,5 +160,41 @@ public class Environment {
 		}
 		//TODO: everything
 		return null;
+	}
+	
+	/**
+	 * Given a 2D string array, creates all perumtaions of the strings and
+	 * returns it as a new 2d String array.
+	 * 
+	 * @param toPermute
+	 * @return
+	 */
+	private static String[][] permute(String[][] toPermute) {
+		String[][] result = null;
+		String[][] tmp;
+		if (toPermute.length > 2) {
+			tmp = permute(Arrays.copyOfRange(toPermute, 1, toPermute.length));
+		} else {
+			result = new String[toPermute[1].length * toPermute[0].length][2];
+			int k = 0;
+			for (int i = 0; i < toPermute[0].length; i++) {
+				for (int j = 0; j < toPermute[1].length; j++) {
+					result[k][0] = toPermute[0][i];
+					result[k][1] = toPermute[1][j];
+					k++;
+				}
+			}
+			return result;
+		}
+		result = new String[toPermute[0].length * tmp.length][tmp[0].length + 1];
+		int k = 0;
+		for (int i = 0; i < toPermute[0].length; i++) {
+			for (int j = 0; j < tmp.length; j++) {
+				result[k][0] = toPermute[0][i];
+				System.arraycopy(tmp[j], 0, result[k], 1, tmp[0].length);
+				k++;
+			}
+		}
+		return result;
 	}
 }
