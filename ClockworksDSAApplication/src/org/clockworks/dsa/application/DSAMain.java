a0e898f5247eaade80a6bd149eba76e16a7801ec
TANGUY Arnaud <arn.tanguy@gmail.com>
2013-03-25 22:11:41 +0000

    Cleanup app

--- ClockworksDSAApplication/src/org/clockworks/dsa/application/DSAMain.java
+++ ClockworksDSAApplication/src/org/clockworks/dsa/application/DSAMain.java
@@ -2,25 +2,23 @@ package org.clockworks.dsa.application;
 
 import android.content.Context;
 
-public class DSAMain extends Thread{
+public class DSAMain extends Thread {
 	private String simulationResults, environmentID, segmentID;
 	private boolean done;
 	private ServerContacter requester;
-	private PythonService processor;
-	
-	public DSAMain(String server, Context context){
+
+	public DSAMain(String server, Context context) {
 		this.requester = new ServerContacter(server, context);
-		this.processor = new PythonService();
 	}
-	
-	public void run(){
+
+	public void run() {
 		done = false;
-		//discard any old results from last run
+		// discard any old results from last run
 		simulationResults = null;
 		environmentID = null;
 		segmentID = null;
-		while(!done){
-			while(!requester.onAllowedNetwork()){
+		while (!done) {
+			while (!requester.onAllowedNetwork()) {
 				try {
 					Thread.sleep(100);
 				} catch (InterruptedException e) {
@@ -28,31 +26,31 @@ public class DSAMain extends Thread{
 					e.printStackTrace();
 				}
 			}
-			
-			RTPResponse serverResponse = requester.sendRTPPing(simulationResults, environmentID, segmentID);
+
+			RTPResponse serverResponse = requester.sendRTPPing(
+					simulationResults, environmentID, segmentID);
 			try {
 				Thread.sleep(30000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			if(serverResponse.getResponseCode() == 200){
+			if (serverResponse.getResponseCode() == 200) {
 				String simulationPath = serverResponse.getSimulationFilePath();
 				// TODO Create thread for processing the python script
 				boolean abort = false;
 				// TODO while not finished processing and abort is false
-					// TODO If timeout reached
-						if(requester.onAllowedNetwork()){
-							requester.sendRTOPing();
-						}
-						else{
-							abort = true;
-						}
+				// TODO If timeout reached
+				if (requester.onAllowedNetwork()) {
+					requester.sendRTOPing();
+				} else {
+					abort = true;
+				}
 			}
 		}
 	}
-	
-	public void exit(){
+
+	public void exit() {
 		done = true;
 	}
 
